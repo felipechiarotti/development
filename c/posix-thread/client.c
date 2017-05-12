d5ae005f4bb12c42c8310e95c37edd043b20b89f
Felipe Chiarotti <felipechi97@gmail.com>
2017-05-12 16:32:50 -0300

    Modifications

--- c/posix-thread/client.c
+++ c/posix-thread/client.c
@@ -11,29 +11,20 @@
 #include <string.h>
  
 int connect_client(char* ip, int port){
-    /*Descritor do cliente*/
     int clienteSockfd;
-    /*Estrutura do Cliente*/
     struct sockaddr_in serv_addr;
-    /*Tamanho da estrutura*/
     socklen_t addrlen = sizeof (serv_addr);
  
-    /*Socket familia TCP declarado SOCK_STREAM e criado*/
     clienteSockfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (clienteSockfd < 0)
     {
         printf("Erro no Socket\n");
         exit(1);
     }
-    /*Zera a estrutura*/
     bzero((char *) & serv_addr, sizeof (serv_addr));
-    /*Seta a familia*/
     serv_addr.sin_family = AF_INET;
-    /*Define o IP nesse caso o localhost*/
     serv_addr.sin_addr.s_addr = inet_addr(ip);
-    /*Define a porta de conexao*/
     serv_addr.sin_port = htons(port);
-   /*Faz a conexao com o servidor*/
    if(connect(clienteSockfd,(struct sockaddr *)&serv_addr, addrlen) < 0)
      {
         printf("Erro ao conectar-se ao servidor\n");
@@ -44,20 +35,14 @@ int connect_client(char* ip, int port){
 
 void run_client(int clienteSockfd)
 {
-  /*Buffer de dados a ser mandado para o servidor*/
   char buffer_para_servidor[256];
   printf("Digite uma mensagem para o servidor: ");
    do {
  
         scanf("%s",buffer_para_servidor);
         fflush(stdin);
-        /*Escreve para o servidor*/
- 
         write(clienteSockfd, buffer_para_servidor, sizeof (buffer_para_servidor));
- 
-      /*Mensagem para sair*/
     } while (strcmp(buffer_para_servidor, "sair") != 0);
-    /**Encerra o descritor*/
     close(clienteSockfd);
 }
 
