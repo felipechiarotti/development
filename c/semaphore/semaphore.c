8cd973847b19a1fe1311f4907c7517177bd61069
Felipe Chiarotti <felipechi97@gmail.com>
2017-06-06 21:18:49 -0300

    C Semaphore

--- c/semaphore/semaphore.c
+++ c/semaphore/semaphore.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Semaphore{
-    pthread_t fila[50];
-    int estado = 0;
-    int fila_size = -1;
+    pthread_t *fila;
+    int estado;
+    int fila_size;
 };
 
 void semaphore_init(struct Semaphore s){
+    s.fila = (pthread_t*) malloc(50 * sizeof(pthread_t))
     s.estado = 0;
     s.fila_size = -1;
 }
 
+// void upgrade_fila(struct Semaphore s){
+//     s.fila = (pthread_t*) realloc(s.fila, 5 * sizeof(pthread_t));
+// }
+
 void lock(struct Semaphore s, pthread_t thread){
     s.fila_size++;
-    s.fila[s.fila_size] = thread;
-    pthread_t locked = thread;
+    pthread_t locked = s.fila[s.fila_size] = thread;
     while(locked != s.fila[0]);
     s.estado = 1;
 }
